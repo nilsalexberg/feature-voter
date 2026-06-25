26fd47e2599ea7e7114bd6513abf36d094d7877a
Nils Alexandre Bergsten <nilsalexberg@gmail.com>
2026-06-25 17:38:01 -0300

    feat: configure Django admin to use email for authentication and user creation

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,42 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class EmailUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+class EmailUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class EmailUserAdmin(UserAdmin):
+    add_form = EmailUserCreationForm
+    form = EmailUserChangeForm
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, EmailUserAdmin)
