c7c054da1db5d17a4cb366441eb9aaff64edebf3
Nils Alexandre Bergsten <nilsalexberg@gmail.com>
2026-06-25 17:42:26 -0300

    feat: install django-unfold and configure as the admin interface theme

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
